332a1e3cddc0eb2bc2986e96cc0fbd009f7a25fe
fabianmolina <fabianmolina@localhost>
2024-02-24 18:58:02 -0600

    Puse un sout

--- src/main/java/com/mycompany/proyecto_sga/Proyecto_SGA.java
+++ src/main/java/com/mycompany/proyecto_sga/Proyecto_SGA.java
@@ -11,6 +11,6 @@ package com.mycompany.proyecto_sga;
 public class Proyecto_SGA {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello mepsfaasf!");
     }
 }
