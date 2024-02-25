9b5a1bb486a70740fbfa3130f4d6d46903b0a7a3
fabianmolina <fabianmolina@localhost>
2024-02-24 18:30:46 -0600

    Puse un sout

---
+++ src/main/java/com/mycompany/proyecto_sga/Proyecto_SGA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_sga;
+
+/**
+ *
+ * @author fabianmolina
+ */
+public class Proyecto_SGA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
